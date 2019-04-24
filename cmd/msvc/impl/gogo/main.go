81132aaf0ff48414b98bd191f1757d8925d4321b
go-packg <valeri_daode@mail.ru>
2019-04-24 13:58:19 +0300

    gogo

--- cmd/msvc/impl/gogo/main.go
+++ cmd/msvc/impl/gogo/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
-	_ "fmt"
+	"os"
+	service "github.com/cloudnativego/gogo-service/service"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	
+	server := service.NewServer()
+	server.Run(":" + port)
 }
